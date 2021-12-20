55ac49a1f2a7c7dfb2026601a31a00e11d1f7476
Grant <kornish10@live.com>
2021-12-20 10:33:04 +0000

    Initial Commit

---
+++ Dictionary_Practice.py
@@ -0,0 +1,4 @@
+dictionary = {"Paul" : "Good Guy", "Ewen" : "Wank", "Jack" : "Good Guy", "Steven" : "Double Wank"} #  Dictionary is a list of Keys(L:) and Values(:R)
+key = input("Good Guy or Wank?") # Requesting a User-Inputed Key
+value=dictionary[key] # Obtaining the value for the User-Requested Key
+print (key + " is a " + value) #Profit
